8ebc7cff1c33a80c682908dbef357b455e02ea13
altarulas <altarulas09@gmail.com>
2024-02-23 16:52:54 +0300

    commit: add callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,6 +2,8 @@ import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
   const session = await auth();
+
+  session?.user.id;
   return (
     <div>
       {JSON.stringify(session)}
